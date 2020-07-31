fee4731f7d6dc7881d0c8b2cb0f522655a446f0b
kosowskie <kosowskie@protonmail.com>
2020-07-31 18:30:54 +0200

    dodanie dyrektyw ngClass oraz ngIf

--- src/app/cars/cars-list/cars-list.component.ts
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -21,6 +21,45 @@ export class CarsListComponent implements OnInit {
       },
       cost: 300,
       isFullyDamaged: true
+    },
+    {
+      id: 2,
+      model: 'Peugeot 306',
+      plate: 'GD223F',
+      deliveryDate: '22-04-2017',
+      deadline: '05-05-2016',
+      client: {
+        firstName: 'Andrzej',
+        surname: 'Lepper'
+      },
+      cost: 50,
+      isFullyDamaged: false
+    },
+    {
+      id: 3,
+      model: 'Skoda Fabia',
+      plate: 'GD322F',
+      deliveryDate: '21-05-2017',
+      deadline: '05-05-2019',
+      client: {
+        firstName: 'Leszek',
+        surname: 'Miller'
+      },
+      cost: 500,
+      isFullyDamaged: false
+    },
+    {
+      id: 4,
+      model: 'Fiat Punto',
+      plate: 'GD242A',
+      deliveryDate: '22-11-2017',
+      deadline: '05-05-2018',
+      client: {
+        firstName: 'Krzysztof',
+        surname: 'Tyniec'
+      },
+      cost: 800,
+      isFullyDamaged: true
     }
   ];
   constructor() { }
