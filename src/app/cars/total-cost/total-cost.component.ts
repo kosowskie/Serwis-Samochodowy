b9d459d001f852556da72914a9830f1589f00e41
kosowskie <kosowskie@protonmail.com>
2020-07-31 21:14:34 +0200

    total-cost component

---
+++ src/app/cars/total-cost/total-cost.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-total-cost',
+  templateUrl: './total-cost.component.html',
+  styleUrls: ['./total-cost.component.less']
+})
+export class TotalCostComponent {
+  @Input() totalCost: number;
+}
